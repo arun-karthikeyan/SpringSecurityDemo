ffdbb59ee1050fb3e8d4f54da0196dbc5187975e
the3drevolution <akarthi2@asu.edu>
2016-06-28 17:24:11 -0700

    OAuth server update

--- SpringSecurityDemo/build/resources/main/static/js/hello.js
+++ SpringSecurityDemo/build/resources/main/static/js/hello.js
@@ -1,84 +1,37 @@
-angular.module('securapp', ['ngRoute', 'securapp.controllers'])
-.config(function($routeProvider){
+angular.module('securapp', [ 'ngRoute', 'securapp.controllers' ]).config(function($routeProvider) {
 
-$routeProvider.when('/', {
-    templateUrl: './views/home.html',
-    controller: 'HomeController'
-}).when('/login', {
-    templateUrl: './views/login.html',
-    controller: 'LoginController'
-}).otherwise('/');
+	$routeProvider.when('/', {
+		templateUrl : './views/home.html',
+		controller : 'HomeController'
+	}).otherwise('/');
 
 });
 
 angular.module('securapp.controllers', [])
-.controller('HomeController',['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
+.controller('IndexController',['$rootScope', '$http', '$location', '$route', '$scope',function($rootScope, $http, $location, $route, $scope) {
 
-    if($rootScope.authenticated) {
-    $http.get('/token').then(function(response) {
-        $http({
-//            url : '/resource/',
-            url : '/resource/',
-            method : 'GET',
-            headers: {
-            'X-Auth-Token' : response.data.token
-            }
-            }).then(function(response) {
-                $scope.greeting = response.data;
-            });
-    });
-}
-    //$scope.greeting = {id: '123', content: 'Hello From Angular'};
-}])
+    $scope.tab = {selection: 0};
+	$http.get('user/').then(function(response) {
+		if (response.data.name) {
+			$rootScope.authenticated = true;
+		} else {
+			$rootScope.authenticated = false;
+		}
+	}, function() {
+		$rootScope.authenticated = false;
+	});
 
-.controller('LoginController', ['$scope', '$rootScope', '$http', '$location', function($scope, $rootScope, $http, $location){
-    $scope.user = {name: '', password: ''};
+	$scope.credentials = {};
 
-    var authenticate = function(credentials, callback) {
-        console.log(credentials);
-        var headers = credentials ? {authorization: 'Basic '+btoa(credentials.name+':'+credentials.password)} : {};
-        console.log(headers);
-        $http.get('/user', {headers: headers})
-        .then(function(resp){
-            if(resp.data.name) {
-                $rootScope.authenticated = true;
-            }else {
-                $rootScope.authenticated = false;
-            }
-            callback && callback();
+	$scope.logout = function() {
+		$http.post('logout', {}).finally(function() {
+			$rootScope.authenticated = false;
+			$location.path("/");
+		});
+	}
 
-        }, function(resp){
-            $rootScope.authenticated = false;
-            callback && callback();
-        });
-    };
-
-    //authenticate();
-
-    $scope.login = function(){
-        authenticate($scope.user, function(){
-            if($rootScope.authenticated) {
-                $location.path("/");
-                $scope.error = false;
-            } else {
-                $location.path("/login");
-                $scope.error = true;
-            }
-        })
-    };
-}])
-
-.controller('IndexController', ['$rootScope', '$scope', '$http', '$location', function($rootScope, $scope, $http, $location){
-    $scope.logout = function() {
-        console.log("logout function called");
-        $http.post('/logout', {}).then(function(resp){
-        console.log("/logout success");
-        }, function(resp){
-        console.log("/logout error");
-        }).finally(function() {
-            console.log("/logout finally")
-            $rootScope.authenticated = false;
-            $location.path('/');
-        });
-    };
-}]);
+}]).controller('HomeController', function($http, $scope) {
+	$http.get('resource/').then(function(response) {
+		$scope.greeting = response.data;
+	})
+});
--- SpringSecurityDemo/src/main/java/demo/UIApplication.java
+++ SpringSecurityDemo/src/main/java/demo/UIApplication.java
@@ -2,80 +2,43 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpSession;
-import java.security.Principal;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Created by i855631 on 6/9/16.
  */
 @SpringBootApplication
-@RestController
 @EnableZuulProxy
-public class UIApplication {
+@EnableOAuth2Sso
+public class UIApplication extends WebSecurityConfigurerAdapter {
 
-    @Configuration
-    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .httpBasic()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/index.html", "/views/home.html", "/views/login.html","/", "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and().logout().logoutSuccessUrl("/")
-                    .and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
-                    .csrf().csrfTokenRepository(csrfTokenRepository());
-        }
-        //because angular expects the csrf token to be in this format
-        private CsrfTokenRepository csrfTokenRepository() {
-            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-            repository.setHeaderName("X-XSRF-TOKEN");
-            return repository;
-        }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/index.html", "/views/home.html","/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+                .csrf().csrfTokenRepository(csrfTokenRepository());
 
     }
 
-//    @RequestMapping("/login")
-//    public
-
-    @RequestMapping("/user")
-    public Principal user(Principal user){
-        return user;
+    //because angular expects the csrf token to be in this format
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
     }
 
-    @RequestMapping("/token")
-    @ResponseBody
-    public Map<String, String> token(HttpSession session) {
-        return Collections.singletonMap("token", session.getId());
-    }
 
-//    @RequestMapping("/resource")
-//    public Map<String, Object> res(){
-//        Map<String, Object> model = new HashMap<String, Object>();
-//        model.put("id", UUID.randomUUID().toString());
-//        model.put("content", new String("Hello World"));
-//        return model;
-//    }
 
     public static void main(String[] args) throws Exception{
         SpringApplication.run(UIApplication.class, args);
